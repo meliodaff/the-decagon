b970307e56c7703aa4cc8396704c7497a0f247ee
Jv Bialen <162081127+meliodaff@users.noreply.github.com>
2026-02-11 20:22:39 +0800

    change the ten minds into diverse minds

--- app/page.tsx
+++ app/page.tsx
@@ -142,7 +142,7 @@ export default function Home() {
       <main className=" mx-auto font-sans">
         <section className="w-[80%] min-w-[320px] max-w-[1200px] mx-auto p-4 md:p-12 text-center mb-60 mt-40 my-60 md:mb-72 md:mt-30">
           <h1 className="fade-in-text drop-shadow-2xl text-6xl md:text-8xl font-black bg-linear-to-b sm:bg-linear-to-r from-cyan-500 to-fuchsia-500 text-transparent bg-clip-text mb-4">
-            Ten Minds, One Mission
+            Diverse Minds, One Mission
           </h1>
           <TextAnimate text="to Create Solutions that Work!" />
         </section>
